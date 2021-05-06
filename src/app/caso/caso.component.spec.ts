7bdbbe360991643e99c2050fc70b2e1dc873ebb8
mauricio.belusso <mauricio.belusso@senior.com.br>
2021-05-06 16:45:14 -0300

    Corrigindo erros de compilação

--- src/app/caso/caso.component.spec.ts
+++ src/app/caso/caso.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { MarcaComponent } from './marca.component';
+import { CasoComponent } from './caso.component';
 
 describe('ProdutoraComponent', () => {
-  let component: MarcaComponent;
-  let fixture: ComponentFixture<MarcaComponent>;
+  let component: CasoComponent;
+  let fixture: ComponentFixture<CasoComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MarcaComponent ]
+      declarations: [ CasoComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MarcaComponent);
+    fixture = TestBed.createComponent(CasoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
